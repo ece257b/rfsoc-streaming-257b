8915ea3025f356e21f741d795bbbe14578840e5e
Kishore-1R <winkishore.raj@gmail.com>
2025-04-04 09:07:18 -0700

    Adding HLS codes for counter and latest version of packetizer

---
+++ Vitis HLS codes/Counter/loop_var.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "loop_var.h"
+/*
+//din_t A[N], dsel_t width
+ap_int<8> loop_var() {
+
+  //dout_t out_accum=0;
+  //dsel_t x;
+  static ap_int<8> count=0;
+  count++;
+
+  LOOP_X:for (x=0;x<width; x++) {
+      out_accum += A[x];
+  }
+
+  return count;
+}
+*/
+/*
+#include <hls_stream.h>
+#include <ap_int.h>
+
+void loop_var(hls::stream<ap_uint<8>> &out_stream) {
+#pragma HLS INTERFACE mode=axis register_mode=both port=out_stream register
+#pragma HLS INTERFACE mode=ap_ctrl_none port=return
+    //#pragma HLS INTERFACE axis port=out_stream
+    //#pragma HLS INTERFACE s_axilite port=reset
+    //#pragma HLS INTERFACE s_axilite port=return
+    //#pragma HLS PIPELINE II=1
+
+    static ap_uint<8> count = 0;
+
+    count++;
+
+    out_stream.write(count);  // Send count over AXI Stream
+}
+*/
+
+#include <hls_stream.h>
+#include <ap_axi_sdata.h>
+#include <ap_int.h>
+
+typedef ap_axis<64,0,0,0> axis_word;
+
+//struct axis_word {
+//    ap_uint<64> data;
+//    ap_uint<8>  keep;
+//    ap_uint<1>  last;
+//};
+
+void loop_var(hls::stream<axis_word> &out_stream) {
+#pragma HLS INTERFACE mode=axis register_mode=both port=out_stream register
+#pragma HLS INTERFACE ap_ctrl_none port=return
+
+    static ap_uint<64> count = 0;
+    axis_word output_word;
+    if(count<256){
+    	count++;
+    }
+    else{
+    	// Reset counter
+    	count = 0;
+    }
+
+    output_word.data = count;
+    output_word.keep = 0xFF;  // All bytes are valid
+    output_word.last = (count == 0xFF) ? 1 : 0;  // Set TLAST when count reaches maximum value
+
+    out_stream.write(output_word);
+}
---
+++ Vitis HLS codes/Counter/loop_var_test.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "loop_var.h"
+
+#include <hls_stream.h>
+
+int main () {
+  din_t A[N];
+  dsel_t x=0;
+  //ap_uint<8> accum;
+  hls::stream<ap_uint<8>> accum;
+  
+  int i, retval=0;
+  ofstream FILE;
+
+  for(i=0; i<N;++i) {
+    A[i]=i;
+  }
+  // Save the results to a file
+  FILE.open ("result.dat");
+
+  // Call the function
+  for(i=0; i<N;++i) {
+    x=i;
+    //accum = loop_var();
+    loop_var(accum, false);
+    ap_uint<8> val = accum.read();
+    FILE << static_cast<int>(val) << endl;
+  }
+  FILE.close();
+  
+  // Compare the results file with the golden results
+  retval = system("diff --brief -w result.dat result.golden.dat");
+  if (retval != 0) {
+    cout << "Test failed  !!!" << endl; 
+    retval=1;
+  } else {
+    cout << "Test passed !" << endl;
+  }
+
+  // Return 0 if the test passed
+  return retval;
+}
+
